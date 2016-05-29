15e3ec42556f770eb4fa8ac75870a07eb9ee34df
Brian Fung <bfung11@gmail.com>
2016-05-28 18:14:37 -0700

    Adding table for column labels

---
+++ src/java/Table.java
@@ -0,0 +1,16 @@
+/**
+ * Table has the column names for the tables
+ *
+ * @author Brian Fung
+ */
+
+import java.time.*;
+import java.sql.*;
+
+public class Table {
+   public final static String RESERVATION_ID = "reservation_id";
+   public final static String FLOOR_NUMBER = "floor_num";
+   public final static String ROOM_NUMBER = "room_num";
+   public final static String START_DATE = "start_date";
+   public final static String END_DATE = "end_date";
+}
