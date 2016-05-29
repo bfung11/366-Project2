07f472c503d11f87394988fd7fda5e73d6ec1c77
Brian Fung <bfung11@gmail.com>
2016-05-28 18:47:32 -0700

    Adding more fields to be printed for reservation

--- src/java/Reservation.java
+++ src/java/Reservation.java
@@ -9,19 +9,25 @@ import java.sql.*;
 
 public class Reservation {
    private int reservationID;
-   private int floorNumber;
-   private int roomNumber;
    private String startDate;
    private String endDate;
+   private int viewType;
+   private int roomType;
+   private int floorNumber;
+   private int roomNumber;
 
    public void setReservationID(int reservationID) { this.reservationID = reservationID; }
    public int getReservationID() { return reservationID; }
-   public void setFloorNumber(int floorNumber) { this.floorNumber = floorNumber; }
-   public int getFloorNumber() { return floorNumber; }
-   public void setRoomNumber(int roomNumber) { this.roomNumber = roomNumber; }
-   public int getRoomNumber() { return roomNumber; }
    public void setStartDate(String date) { startDate = date; }
    public String getStartDate() { return startDate; }
    public void setEndDate(String date) { endDate = date; }
    public String getEndDate() { return endDate; }
+   public void setFloorNumber(int floorNumber) { this.floorNumber = floorNumber; }
+   public int getFloorNumber() { return floorNumber; }
+   public void setRoomNumber(int roomNumber) { this.roomNumber = roomNumber; }
+   public int getRoomNumber() { return roomNumber; }
+   public void setViewType(int viewType) { this.viewType = viewType; }
+   public int getViewType() { return viewType; }
+   public void setRoomType(int roomType) { this.roomType = roomType; }
+   public int getRoomType() { return roomType; }
 }
