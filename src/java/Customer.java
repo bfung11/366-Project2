ef9d884e3886415cb468569e592be9460e58aacd
Brian Fung <bfung11@gmail.com>
2016-05-28 18:15:10 -0700

    Adding queries for customer

--- src/java/Customer.java
+++ src/java/Customer.java
@@ -5,10 +5,15 @@
  * @author Justin Zaman
  */
 
+import javax.el.ELContext;
+import javax.faces.context.FacesContext;
 import java.time.*;
 import java.sql.*;
+import java.util.*;
+
 
 public class Customer {
+   private String username;
    private int reservationID;
    private int floorNumber;
    private int roomNumber;
@@ -26,15 +31,80 @@ public class Customer {
    public void setEndDate(String date) { endDate = LocalDate.parse(date); }
    public LocalDate getEndDate() { return endDate; }
 
-   public void getReservation() {
-      //TODO : Add queries
+   public void makeReservation() {
+      try {
+         String query = 
+            "INSERT INTO reservations " + 
+            "VALUES " + 
+               "(DEFAULT, " + 
+                 "'" + username + "', " +
+                       floorNumber + "', " +
+                       roomNumber + ", " + 
+                 "'" + startDate + "', " + 
+                 "'" + endDate + "', " + 
+                 "NULL, NULL)";
+         DBConnection connection = new DBConnection();
+         connection.executeUpdate(query);
+      }
+      catch (Exception e) {
+         e.printStackTrace();
+      }
    }
 
-   public void checkReservations() {
-      //TODO : Add queries
+   public ArrayList<Reservation> checkReservations() {
+      getUsername();
+
+      ArrayList<Reservation> list = new ArrayList<Reservation>();
+      try {
+         String query = 
+            "SELECT * " + 
+            "FROM reservations " + 
+            "WHERE reservation_id = " + reservationID;  
+         DBConnection connection = new DBConnection();
+         ResultSet result = connection.executeQuery(query);
+
+         while (result.next()) {
+            Reservation reservation = new Reservation();
+            reservation.setReservationID(result.getInt(Table.RESERVATION_ID));
+            reservation.setFloorNumber(result.getInt(Table.FLOOR_NUMBER));
+            reservation.setRoomNumber(result.getInt(Table.ROOM_NUMBER));
+            LocalDate startDate = 
+               LocalDate.parse(result.getDate(Table.START_DATE).toString());
+            reservation.setStartDate(startDate.toString());
+            LocalDate endDate = 
+               LocalDate.parse(result.getDate(Table.END_DATE).toString());
+            reservation.setEndDate(endDate.toString());
+            list.add(reservation);
+         }
+      }
+      catch (Exception e) {
+         e.printStackTrace();
+      }
+
+      return list;
+   }
+
+   private void getUsername() {
+      ELContext elContext = FacesContext.getCurrentInstance().getELContext();
+      Login login = (Login) elContext.getELResolver().getValue(elContext, null, "login");
+      username = login.getUsername();
    }
 
    public void cancelReservations() {
-      //TODO : Add queries
+      try {
+         String query = 
+            "DELETE FROM bills " + 
+            "WHERE reservation_id = " + reservationID;
+         DBConnection connection = new DBConnection();
+         connection.executeUpdate(query);
+
+         query = 
+            "DELETE FROM reservations " + 
+            "WHERE reservation_id = " + reservationID;
+         connection.executeUpdate(query);
+      }
+      catch (Exception e) {
+         e.printStackTrace();
+      }
    }
 }
