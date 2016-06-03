ae6af8aef7126953b7e3b7ac490edf4c6dffb18d
Brian Fung <bfung11@gmail.com>
2016-06-02 17:40:26 -0700

    Adding CHECK_OUT_DATE static string for table column

--- src/java/Table.java
+++ src/java/Table.java
@@ -12,6 +12,7 @@ public class Table {
    public final static String RESERVATION_ID = "reservation_id";
    public final static String START_DATE = "start_date";
    public final static String END_DATE = "end_date";
+   public final static String CHECK_OUT_DATE = "check_out_date";
    public final static String FLOOR_NUMBER = "floor_num";
    public final static String ROOM_NUMBER = "room_num";
    public final static String ROOM_TYPE = "room_type";
